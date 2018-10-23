c4ab7556ba6f4af7c14b13b7e4731817dac06a5d
boyfox <mr.boyfox@gmail.com>
2018-10-23 16:52:47 +0500

    1. Sorting by pinned items

--- app/src/main/java/com/unrealmojo/hamsters/helpers/customs/CharacterItemDecaration.java
+++ app/src/main/java/com/unrealmojo/hamsters/helpers/customs/CharacterItemDecaration.java
@@ -21,7 +21,7 @@ public class CharacterItemDecaration extends RecyclerView.ItemDecoration {
         outRect.bottom = mSpace;
 
         // Add top margin only for the first item to avoid double space between items
-        if (parent.getChildAdapterPosition(view) == 0)
+        if (parent.getChildAdapterPosition(view) == 0 || parent.getChildAdapterPosition(view) == 1)
             outRect.top = 2 * mSpace;
     }
 }
